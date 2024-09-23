2e2795be0b91d028e2e3588c591a5da0f494e828
baebaeseonhuii <qotjsgml3@naver.com>
2024-09-23 12:17:21 +0900

    start project

---
+++ src/main/java/com/seonhui/app/Gdj83ShenanigansApplication.java
@@ -0,0 +1,13 @@
+package com.seonhui.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gdj83ShenanigansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gdj83ShenanigansApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/seonhui/app/home/HomeController.java
@@ -0,0 +1,15 @@
+package com.seonhui.app.home;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String home() throws Exception{
+		return "index";
+	}
+}
